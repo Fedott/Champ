d4144e2b20f5b16eea985cacac543cc015c28129
fedot <fedotru@gmail.com>
2009-11-27 18:34:52 +0300

    Добавил функционал в меню. Нужно допиливать стили срочно

---
+++ application/views/menu.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');?>
+<p>
+	<?=$user->username?> <?=html::anchor('logout', 'Выйти', array('class' => 'logout'));?>
+</p>
+<?if(count($teams)):?>
+<p>
+	Ваши команды:
+</p>
+<ul>
+<?foreach($teams as $team):?>
+	<li>
+		<?=$team->team->name;?> (<?=$team->table->name;?>)
+	</li>
+<?endforeach;?>
+</ul>
+<?endif;?>
--- application/widgets/menu.php
+++ application/widgets/menu.php
@@ -6,7 +6,12 @@
 		{
 			if(Auth::instance()->logged_in())
 			{
-				$tpl = "Привет, ".Auth::instance()->get_user()->username."!";
+				$user = Auth::instance()->get_user();
+				$teams = ORM::factory('line')->where(array('user_id' => $user->id))->find_all();
+
+				$tpl = new View('menu');
+				$tpl->user = $user;
+				$tpl->teams = $teams;
 			}
 			else
 			{
