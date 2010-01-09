f9df22ea28c678d0c2d1e36fd746117761d76b0f
Fedot <fedotru@gmail.com>
2010-01-09 19:55:12 +0300

    Изменил меню, теперь есть только один "Вид" меню, тоесть логика находиться в одном файле

--- application/views/menu.php
+++ application/views/menu.php
@@ -1,22 +1,28 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');?>
-<p>
-	<?=$user->username?> <?=html::anchor('logout', 'Выйти', array('class' => 'logout'));?>
-</p>
+<?if($user != NULL):?>
+	<p>
+		<?=$user->username?> <?=html::anchor('logout', 'Выйти', array('class' => 'logout'));?>
+	</p>
 
-<ul class="vmenu">
-	<li><?=html::anchor('match', 'Матчи');?></li>
-	<li><?=html::anchor('tournament', "Турниры");?></li>
-</ul>
+	<ul class="vmenu">
+		<li><?=html::anchor('match', 'Матчи');?></li>
+		<li><?=html::anchor('tournament', "Турниры");?></li>
+	</ul>
 
-<?if(count($teams)):?>
-<p>
-	Ваши команды:
-</p>
-<ul class="my_teams">
-<?foreach($teams as $team):?>
-	<li>
-		<?=$team->team->name;?> (<?=html::anchor('tournament/view/'.$team->table->url, $team->table->name);?>)
-	</li>
-<?endforeach;?>
-</ul>
+	<?if(count($teams)):?>
+		<p>
+			Ваши команды:
+		</p>
+		<ul class="my_teams">
+		<?foreach($teams as $team):?>
+			<li>
+				<?=$team->team->name;?> (<?=html::anchor('tournament/view/'.$team->table->url, $team->table->name);?>)
+			</li>
+		<?endforeach;?>
+		</ul>
+	<?endif;?>
+<?else:?>
+	<p>
+		<?=html::anchor('login', 'Войдите')." на сайт"."<br>Или ".html::anchor('reg', 'зарегистрируйтесь');?>
+	</p>
 <?endif;?>
